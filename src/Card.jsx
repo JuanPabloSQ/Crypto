281f912d540da427867b36b8b6020148e1199e2c
JP Sherka <jp.sherka@gmail.com>
2023-12-12 02:24:15 -0300

    add rounding

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,12 @@ import "./Card.css"
 const Card = ({baseCurrency, targetCurrency, exchangeRate}) => {
 
   const formatNumber = (number) => {
-    const formattedNumber = new Intl.NumberFormat("es-CL", {style: "currency",currency: targetCurrency, minimumFractionDigits: 0,maximumFractionDigits: 2,
+    const formattedNumber = new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: targetCurrency, 
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+    roundingMode: 'floor'
     }).format(number);
     return formattedNumber ;
   };
