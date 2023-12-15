6dc238a7b36bd0e9d7479091e1051f25f8dba93a
JP Sherka <jp.sherka@gmail.com>
2023-12-15 19:39:06 -0300

    add a

--- src/Card.jsx
+++ src/Card.jsx
@@ -21,36 +21,36 @@ const Card = ({ baseCurrency, targetCurrency, exchangeRate, item, isLoading }) =
 
   return (
     <div className="card">
-       {isLoading ? (
+      {isLoading ? (
         <div className="skeleton-card">
-        <h2>
-        <Skeleton variant="text" 
-        sx={{ fontSize: '1rem' }}  
-        width={130}
-        height={25}
-       /></h2>
-       <h1><Skeleton variant="text" 
-        sx={{ fontSize: '1rem' }}  
-        width={100}
-        height={25}
-       /></h1>
-       <p>
-       <Skeleton variant="text" 
-        sx={{ fontSize: '1rem' }}  
-        width={130}
-        height={25}
-       />
-       </p>
-       </div>
+          <h2>
+            <Skeleton variant="text"
+              sx={{ fontSize: '1rem' }}
+              width={130}
+              height={25}
+            /></h2>
+          <h1><Skeleton variant="text"
+            sx={{ fontSize: '1rem' }}
+            width={100}
+            height={25}
+          /></h1>
+          <p>
+            <Skeleton variant="text"
+              sx={{ fontSize: '1rem' }}
+              width={130}
+              height={25}
+            />
+          </p>
+        </div>
       ) : (
         <>
-      <h2>{baseCurrency}/{targetCurrency}</h2>
-      <div className="tooltip">
-        <span className="tooltiptext">{formattedExchangeRate} </span>
-        <p>{formattedExchangeRate}   
-        </p>
-      </div>
-      </>
+          <h2>{baseCurrency}/{targetCurrency}</h2>
+          <div className="tooltip">
+            <span className="tooltiptext">{formattedExchangeRate} </span>
+            <p>{formattedExchangeRate}
+            </p>
+          </div>
+        </>
       )}
     </div>
   );
