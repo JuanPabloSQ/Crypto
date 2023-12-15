f6de34ad662a5c662c45cf559b6448bf659e210a
JP Sherka <jp.sherka@gmail.com>
2023-12-15 17:53:01 -0300

    add skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
+import React,{useEffect, useState} from "react";
 import Card from "./Card.jsx"
+import Skeleton from '@mui/material/Skeleton';
+
 
 function App() {
-  return(
-  <div>
-    <Card
-    targetCurrency="CLP"
-    baseCurrency="USD"
-    exchangeRate={100.057} />  
-  </div>  
-  )
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getData = async () => {
+    try{
+      await new Promise(resolve => setTimeout(resolve, 4000));
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect (() => {
+    getData();
+  }, []);
+  
+
+  return (
+    <div>
+      {isLoading ? (
+        <Skeleton variant="rectangular" width={150} height={120} />
+      ) : (
+        <Card
+          targetCurrency="CLP"
+          baseCurrency="USD"
+          exchangeRate={100.057}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,25 @@
-import "./Card.css"
 
-const Card = ({baseCurrency, targetCurrency, exchangeRate}) => {
+import React,{useEffect, useState} from "react";
+import "./Card.css";
 
+
+
+// const [data, setData] = useState ()
+// const [isLoading, setIsLoading] = useState(false)
+
+// const getData
+
+
+const Card = ({ baseCurrency, targetCurrency, exchangeRate, item }) => {
   const formatNumber = (number) => {
     const formattedNumber = new Intl.NumberFormat("es-CL", {
-    style: "currency",
-    currency: targetCurrency, 
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2,
-    roundingMode: 'floor'
+      style: "currency",
+      currency: targetCurrency,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+      roundingMode: "floor",
     }).format(number);
-    return formattedNumber ;
+    return formattedNumber;
   };
 
   const formattedExchangeRate = formatNumber(exchangeRate);
@@ -19,11 +28,17 @@ const Card = ({baseCurrency, targetCurrency, exchangeRate}) => {
     <div className="card">
       <h2>{baseCurrency}/{targetCurrency}</h2>
       <div className="tooltip">
-        <span className="tooltiptext">{formattedExchangeRate}</span>
-        <p>{formattedExchangeRate}</p>
+        <span className="tooltiptext">{formattedExchangeRate} </span>
+        <p>{formattedExchangeRate}   
+        </p>
       </div>
     </div>
   );
 };
 
 export default Card;
+
+
+
+
+
